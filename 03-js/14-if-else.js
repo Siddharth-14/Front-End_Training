676ae9b237ac122c7f755dc621dfaf8fa1b80ab5
Prashanth Puranik <puranik@digdeeper.in>
2021-02-05 14:56:01 +0530

    looping

---
+++ 03-js/14-if-else.js
@@ -0,0 +1,7 @@
+const baseLocation = 'Bangalore';
+
+if( baseLocation === 'Gurugram' ) {
+    console.log( 'DLF Cyberhub' );
+} else if( baseLocation === 'Bangalore' ) {
+    console.log( 'Bagmane Constellation' );
+}
