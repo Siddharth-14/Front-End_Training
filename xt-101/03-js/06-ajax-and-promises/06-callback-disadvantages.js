a91c200d1bc8db5a54079701b89ec6e493fee022
Prashanth Puranik <puranik@digdeeper.in>
2021-02-15 09:59:12 +0530

    christmas tree (callback hell)

--- xt-101/03-js/06-ajax-and-promises/06-callback-disadvantages.js
+++ xt-101/03-js/06-ajax-and-promises/06-callback-disadvantages.js
@@ -4,8 +4,18 @@ function sum( x, y, cb ) {
     }, 3000);
 }
 
-sum( 12, 13, function( result ) {
-    console.log( 'result = ', result );
+// christmas tree (callback hell)
+// code readability is poor with callback pattern
+sum( 12, 13, function( result1 ) {
+    console.log( 'result1 = ', result1 );
+
+    sum( result1, 14, function( result2 ) {
+        console.log( 'result2 = ', result2 );
+
+        sum( result2, 15, function( result3 ) {
+            console.log( 'result3 = ', result3 );
+        });
+    });
 });
 
 // EXERCISE: Use the same sum() function to add 14 once the previous result is obtained. Then use the same sum() function to add 15 to the new result once the new result is obtained.
