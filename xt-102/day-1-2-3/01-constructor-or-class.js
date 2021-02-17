4a0c24c5a18930afae247de8033c20fa61ba2863
Prashanth Puranik <puranik@digdeeper.in>
2021-02-17 10:19:46 +0530

    how prototype of an object is set by default

--- xt-102/day-1-2-3/01-constructor-or-class.js
+++ xt-102/day-1-2-3/01-constructor-or-class.js
@@ -6,5 +6,13 @@ function Person( name, age ) {
     // return this;
 }
 
-const salman = new Person( 'Salman', 21 ); // this -> { __proto__ : { } }
+const salman = new Person( 'Salman', 21 ); // this -> { __proto__ : { constructor : Person } }
 console.log( salman );
+
+const shahrukh = new salman.__proto__.constructor( 'Shah Rukh', 45 );
+
+console.log( salman.__proto__.constructor === Person ); // true
+console.log( salman instanceof Person ); // true
+
+// Check what is there in __proto__
+// Create a User class to represent a uyser
