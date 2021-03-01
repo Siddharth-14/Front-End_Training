7ecaccb97b5805e1c074b21352abfbbdba432a20
Prashanth Puranik <puranik@digdeeper.in>
2021-03-01 11:37:40 +0530

    regular expression patterns

--- xt-102/day-7-8/regular-expressions/19-alternation-and-parenthesis.js
+++ xt-102/day-7-8/regular-expressions/19-alternation-and-parenthesis.js
@@ -5,7 +5,13 @@ console.log( pat.test( 'abxy' ) ); // true
 console.log( pat.test( 'cdxy' ) ); // true
 console.log( pat.test( 'acxy' ) ); // false
 
-const pat2 = /(ab|cd|ef)+/;
+const pat2 = /(ab|cd|ef)+/; // ab|cd|ef is repeated multiple times
+console.log( 'ababxy'.match( pat2 )[0] );
+console.log( 'abcdxy'.match( pat2 )[0] );
+console.log( 'abxycd'.match( pat2 )[0] );
+console.log( 'abcdefabefxyabcdabcdef'.match( pat2 )[0] );
+
+const pat2 = /ab|cd|ef+/; // only f is repeated multiple times
 console.log( 'ababxy'.match( pat2 )[0] );
 console.log( 'abcdxy'.match( pat2 )[0] );
 console.log( 'abxycd'.match( pat2 )[0] );
