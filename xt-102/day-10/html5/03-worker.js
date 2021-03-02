4f6d7e28d7bc661ec77b343d84baef1ca2ce29cd
Prashanth Puranik <puranik@digdeeper.in>
2021-03-02 10:35:48 +0530

    drag n drop, storage, web workers

--- xt-102/day-10/html5/03-worker.js
+++ xt-102/day-10/html5/03-worker.js
@@ -1,20 +1,22 @@
-// function sum( a, b ) {
-//     return a + b;
-// }
-
-function sum() {
-    var result = 0;
-    for( var i = 0; i < 10000000000; i++ ) {
-        for( var j = 0; j < 10000000000; j++ ) {
-            result += i;
-        }
+function nthfibonacci( n ) {
+    if( n == 1 ) {
+        return 0;
     }
+
+    if( n === 2 ) {
+        return 1;
+    }
+
+    return nthfibonacci( n - 1 ) + nthfibonacci( n - 2 );
 }
 
-onmessage = function( event ) {
-    var result = sum( event.data.a, event.data.b );
+onmessage = function( { data: { n } } ) {
+    console.log( 'Received : ', n, '. Now starting to compute...' );
+
+    const result = nthfibonacci( n );
 
     postMessage({
-        result: result
+        n,
+        result
     });
 };
