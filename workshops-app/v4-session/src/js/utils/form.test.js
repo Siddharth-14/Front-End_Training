8e26e0be2b184325c9fe27de89f43c46b12b537d
Prashanth Puranik <puranik@digdeeper.in>
2021-03-10 10:33:46 +0530

    test involving DOM nodes

--- workshops-app/v4-session/src/js/utils/form.test.js
+++ workshops-app/v4-session/src/js/utils/form.test.js
@@ -36,3 +36,33 @@ describe( 'getFormData function', () => {
         })
     });
 });
+
+const showError = ( formControlEl, error ) => {
+    const errorElement = formControlEl.closest( '.form-group' ).querySelector( '.form-control-error' );
+    errorElement.innerHTML = error;
+    errorElement.style.display = 'block';
+};
+
+test( 'showError should show the error passed within the .form-control-error sibling of the passed formControlEl', () => {
+    // arrange
+    document.body.innerHTML = `
+        <form>
+            <div class="form-group">
+                <input type="text" />
+                <div class="form-control-error" style="display: none;"></div>
+            </div>
+        </form>
+    `;
+
+    const formControlEl = document.querySelector( 'input' );
+
+    // act
+    showError( formControlEl, 'Input cannot be empty' );
+
+    // assert
+    expect( document.querySelector( '.form-control-error' ).innerHTML ).toBe( 'Input cannot be empty' );
+    expect( document.querySelector( '.form-control-error' ).style.display ).toBe( 'block' );
+
+    // logging generally to check how HTML has changed
+    console.log( document.body.innerHTML );
+});
--- workshops-app/v4-session/src/js/utils/form2.test.js
+++
@@ -1,7 +0,0 @@
-const showError = ( formControlEl, error ) => {
-    const errorElement = formControlEl.closest( '.form-group' ).querySelector( '.form-control-error' );
-    errorElement.innerHTML = error;
-    errorElement.style.display = 'block';
-};
-
-test( '' )
