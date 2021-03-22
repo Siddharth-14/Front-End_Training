c0176ce754b73718e9bd831e1b6cdca8591b0160
Siddharth Goradia <siddharth.goradia@publicissapient.com>
2021-03-22 16:50:39 +0530

    Workshop commit

--- workshops-app/v5-react/src/components/workshops-list/WorkshopsList.js
+++ workshops-app/v5-react/src/components/workshops-list/WorkshopsList.js
@@ -1,70 +1,90 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class WorkshopsList extends Component {
-    static LOADING = 'LOADING';
-    static LOADED = 'LOADED';
-    static ERROR_LOADING = 'ERROR_LOADING';
+  static LOADING = "LOADING";
+  static LOADED = "LOADED";
+  static ERROR_LOADING = "ERROR_LOADING";
 
-    state = {
-        status: WorkshopsList.LOADING,
-        // workshops: null,
-        // error: null
-    };
+  state = {
+    status: WorkshopsList.LOADING,
+    showDescriptions: [false,false,false,false,false,false,false,false,false,false,false,false],
+    // workshops: null,
+    // error: null
+  };
 
-    render() {
-        const { status, workshops, error } = this.state;
-        let el = null;
+  toggleDescriptions = (id) => {
+    let currDescriptions = [...this.state.showDescriptions];
+    currDescriptions[id-1] = !this.state.showDescriptions[id-1]
+    this.setState((curState) => ({
+      showDescriptions: currDescriptions,
+    }));
+  };
 
-        switch( status ) {
-            case WorkshopsList.LOADING:
-                el = (
-                    <div className="alert alert-primary">We are fetching workshops. Please wait</div>
-                );
-                break;
-            case WorkshopsList.LOADED:
-                el = (
-                    <ul className="my-4">
-                    {
-                        workshops.map( workshop => (
-                            <li key={workshop.id}>
-                                {workshop.name}
-                                <div dangerouslySetInnerHTML={ { __html: workshop.description } }></div>
-                            </li>
-                        ))
-                    }
-                    </ul>
-                );
-                break;
-            case WorkshopsList.ERROR_LOADING:
-                el = (
-                    <div className="alert alert-danger">{error.message}</div>
-                );
-                break;
-            default: 
-                el = null;
-        }
+  render() {
+    const { status, workshops, error, showDescriptions } = this.state;
+    let el = null;
 
-        return (
-            <div className="container">{el}</div>
+    switch (status) {
+      case WorkshopsList.LOADING:
+        el = (
+          <div className="alert alert-primary">
+            We are fetching workshops. Please wait
+          </div>
         );
+        break;
+      case WorkshopsList.LOADED:
+        el = (
+          <div>
+            <ul className="p-0 flex-wrap my-4 flex-column justify-content-between">
+              {workshops.map((workshop) => (
+                <li className="py-4 px-4 card shadow mx-4 my-2" key={workshop.id}>
+                  <h3>{workshop.name}</h3>
+                  {showDescriptions[workshop.id-1] && (
+                    <div
+                      className="mt-3"
+                      dangerouslySetInnerHTML={{ __html: workshop.description }}
+                    ></div>
+                  )}
+                  <button
+                    className="mt-2 btn btn-sm btn-primary"
+                    style={{width:"20%"}}
+                    onClick={()=>this.toggleDescriptions(workshop.id)}
+                  >
+                    {showDescriptions[workshop.id-1] ? "Hidedetails" : "Showdetails"}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+        break;
+      case WorkshopsList.ERROR_LOADING:
+        el = <div className="alert alert-danger">{error.message}</div>;
+        break;
+      default:
+        el = null;
     }
 
-    componentDidMount() {
-        axios.get( `https://workshops-server.herokuapp.com/workshops` )
-            .then(response => {
-                this.setState({
-                    status: WorkshopsList.LOADED,
-                    workshops: response.data
-                });
-            })
-            .catch(error => {
-                this.setState({
-                    status: WorkshopsList.ERROR_LOADING,
-                    error
-                });
-            });
-    }
+    return <div className="container">{el}</div>;
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://workshops-server.herokuapp.com/workshops`)
+      .then((response) => {
+        this.setState({
+          status: WorkshopsList.LOADED,
+          workshops: response.data,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          status: WorkshopsList.ERROR_LOADING,
+          error,
+        });
+      });
+  }
 }
 
 export default WorkshopsList;
